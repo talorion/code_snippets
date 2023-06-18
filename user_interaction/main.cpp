8a1eb7dba448a3a4a37b9e629e15fb7f17a37ed4
talorion <gregor.mayramhof@gmail.com>
2023-06-18 09:46:31 +0200

    print project name and vesrion; clear screen

--- user_interaction/main.cpp
+++ user_interaction/main.cpp
@@ -1,7 +1,9 @@
+#include <cstdlib>
 #include <stdio.h>
-#include <stdlib.h>
+#include "config.h"
 
 int main(){
-    printf("Hello, World!\n");
+    printf("\033[1;1H\033[2J"); // clear screen
+    printf("Starting %s v%s\n",PROJECT_NAME,PROJECT_VER);
     return EXIT_SUCCESS;
 }
