a27f4a63dd6432c7259512e14dda6ed5d6ff8482
Gregor Mayramhof <gregor.mayramhof@aqt.eu>
2023-06-19 12:51:44 +0200

    add spinning cursor

--- user_interaction/main.cpp
+++ user_interaction/main.cpp
@@ -1,25 +1,47 @@
 #include <cstdlib>
 #include <stdio.h>
+#include <thread>
 #include "config.h"
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 constexpr const char * CLEAR_SCREEN_STR ="\033[1;1H\033[2J";
 constexpr const char * CLEAR_LINE_STR ="\33[2K\r";
 
-void clear_screen(void){
-    printf(CLEAR_SCREEN_STR); // clear screen
-}
+namespace clear{
+    void screen(void){
+        printf(CLEAR_SCREEN_STR); // clear screen
+    }
 
-void clear_line(void){
-    printf(CLEAR_LINE_STR); // clear screen
-}
+    void line(void){
+        printf(CLEAR_LINE_STR); // clear screen
+    }
+} // namespace clear
+
+
+namespace spinning_cursor{
+    constexpr size_t count = 5;
+    constexpr const char states[count]={'/','-','\\','|','-' };
+    static int pos=0;
+
+    void tick(){
+        pos = (pos+1) % count;
+        printf("%c", states[pos]);
+        fflush(stdout);
+    }
+} // namespce spinning_cursor
 
 int main(){
-    clear_screen();
+    clear::screen();
     printf("Starting %s v%s\n",PROJECT_NAME,PROJECT_VER);
 
-    for(int i=10 ; i>0; i--){
-        clear_line();
-        printf("Hello %d", i);
+    // spinns a cursor for 10s
+    for(int i = 0; i <100; i++){
+        clear::line();
+        printf("processing... ");
+        spinning_cursor::tick();
+        std::this_thread::sleep_for(100ms);
     }
     printf("\n");
 
