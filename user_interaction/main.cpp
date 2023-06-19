1a7571bcac1d8c780272e57fbf077feeee8e72c6
Gregor Mayramhof <gregor.mayramhof@aqt.eu>
2023-06-19 07:18:04 +0200

    add clear line

--- user_interaction/main.cpp
+++ user_interaction/main.cpp
@@ -2,8 +2,26 @@
 #include <stdio.h>
 #include "config.h"
 
+constexpr const char * CLEAR_SCREEN_STR ="\033[1;1H\033[2J";
+constexpr const char * CLEAR_LINE_STR ="\33[2K\r";
+
+void clear_screen(void){
+    printf(CLEAR_SCREEN_STR); // clear screen
+}
+
+void clear_line(void){
+    printf(CLEAR_LINE_STR); // clear screen
+}
+
 int main(){
-    printf("\033[1;1H\033[2J"); // clear screen
+    clear_screen();
     printf("Starting %s v%s\n",PROJECT_NAME,PROJECT_VER);
+
+    for(int i=10 ; i>0; i--){
+        clear_line();
+        printf("Hello %d", i);
+    }
+    printf("\n");
+
     return EXIT_SUCCESS;
 }
